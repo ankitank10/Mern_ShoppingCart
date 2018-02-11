8270d056c18c68ceaeece5f966dcc209c7a98868
SAPIENT\agu223 <agupta249@sapient.com>
2018-02-11 12:55:09 +0530

    Random

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const passport = require("passport");
 require("./models/users");
 
 const authRoutes = require("./routes/authRoutes");
-//const billingRoutes = require("./routes/billingRoutes");
+const billingRoutes = require("./routes/billingRoutes");
 const Keys = require("./config/keys");
 
 app.use(
@@ -35,19 +35,18 @@ const options = {
 };
 mongoose.connect(database, options);
 authRoutes(app); // This is imp , placing this line on top after const keys gives initialization error
-//billingRoutes(app);
+billingRoutes(app);
 
-if (process.env.NODE_ENV === "Production") {
-  //Express will serve up production assets like
-  //main.js or main.css
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
-  //Express will serve index.html in all other cases if it
-  //doesn't recognise the route
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
