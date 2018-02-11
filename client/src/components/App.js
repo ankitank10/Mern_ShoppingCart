c5e89715d493fedcc04de3c5fb59606cd2787ba7
SAPIENT\agu223 <agupta249@sapient.com>
2018-02-11 12:57:47 +0530

    adding header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-//import Header from "./Header";
+import Header from "./Header";
 //import Landing from "./Landing";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-const Header = () => <h1>Heading</h1>;
 const Landing = () => <h1>Landing</h1>;
 const Surveys = () => <h1>Dashboard</h1>;
 class App extends Component {
